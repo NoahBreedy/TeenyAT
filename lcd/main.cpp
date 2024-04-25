be4a5e31cc27165e378aa91d48864f87c00ca600
William Amos Confer <conferw@sunypoly.edu>
2024-04-25 02:09:53 -0400

    Switch background to near exact 0xDDBBFF

--- lcd/main.cpp
+++ lcd/main.cpp
@@ -153,7 +153,7 @@ int main(int argc, char *argv[])
 
     /* database fabulous lavender for our color model */
 //    initScreen(0xA81); 
-    initScreen(0xBE8); 
+    initScreen(0xD4F); 
 
     std::string fileName = argv[1];
     teenyat t;
