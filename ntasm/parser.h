a9cf01b228e9b152a53ed1de4174240e3b29ca03
Noah Breedy <noahfamily373@gmail.com>
2025-12-18 19:09:22 -0500

    ntasm: enable recursive-descent parser to populate encoding fields
    The recursive-descent parser now populates selected fields in the
    final assembly encoding as instructions are recognized. This lays
    the groundwork for full instruction emission during parse and should
    later help simplify the final binary output stage.

--- ntasm/parser.h
+++ ntasm/parser.h
@@ -3,6 +3,7 @@
 #include <string>
 #include "preprocessor.h"
 #include "token.h"
+#include "teenyat.h"
 
 class Parser {
 public:
@@ -14,21 +15,37 @@ private:
     Preprocessor& pp;
     token current;
 
+    tny_word bin_word_0;
+    tny_word bin_word_1;
+    
+    /* Im making these teeny_words because I don't want the overload */
+    tny_word p_opcode;
+    bool     p_teeny;
+    tny_word p_reg1;
+    tny_word p_reg2;
+    tny_word p_immed;
+    tny_word p_negative;
+
     void advance();
     bool match(token_type t);
+    bool match(token_type t, tny_word* dest);
     void expect(token_type t, const std::string& msg);
+
+    void set_destination(token token, tny_word* dest);
+    tny_word register_to_value(std::string s);
+    tny_word token_to_opcode(token_type t);
 
     void parse_line();
     void parse_statement();
     bool parse_label_line();
     bool parse_code_line();
 
-    bool parse_number();
-    bool parse_raw_value();
-    bool parse_immediate();
-    bool parse_includes_immediate();
-    bool parse_includes_register();
-    bool parse_register_and_immediate();
+    bool parse_number(tny_word* immed);
+    bool parse_raw_value(tny_word* immed);
+    bool parse_immediate(tny_word* immed);
+    bool parse_includes_immediate(tny_word* immed);
+    bool parse_includes_register(tny_word* reg);
+    bool parse_register_and_immediate(tny_word* reg, tny_word* immed);
 
     bool parse_set_instruction();
 };
