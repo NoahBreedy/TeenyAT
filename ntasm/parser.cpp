42898ce1ffdf691003f59c2775c12deca065098a
Noah Breedy <noahfamily373@gmail.com>
2025-12-21 02:07:46 -0500

    ntasm: renamed push_binary to push_binary_instruction
    Im going to make a new function called push_binary_word for when I go
    about implementing the .raw and string functions

--- ntasm/parser.cpp
+++ ntasm/parser.cpp
@@ -48,7 +48,7 @@ bool is_teeny(tny_sword n) {
     return small.instruction.immed4 == n;
 }
 
-void Parser::push_binary() {
+void Parser::push_binary_instruction() {
     /* map our processed values into its associated fields */
 
     bin_word_0.instruction.opcode =  p_opcode.u;
@@ -306,7 +306,7 @@ bool Parser::parse_set_instruction() {
     if(match(T_SET, &p_opcode) && match(T_REGISTER, &p_reg1) && match(T_COMMA)) {
         bool valid_inst = parse_register_and_immediate(&p_reg2, &p_immed);
         if(valid_inst) {
-           push_binary();
+           push_binary_instruction();
            std::cout << p_opcode.u << " " << p_reg1.u << " " << p_reg2.u << " " << p_immed.s << std::endl;
         }
         return valid_inst;
