e66bc2de02824d0facba837c56e4e434482f8737
Noah Breedy <noahfamily373@gmail.com>
2026-06-24 14:19:37 -0400

    tbug: added leroy png and text to the header

--- tbug/src/main.c
+++ tbug/src/main.c
@@ -83,10 +83,21 @@ int main() {
             CLAY(CLAY_ID("Header"), {
                 .layout = { .layoutDirection = CLAY_LEFT_TO_RIGHT,
                             .sizing = { .width = CLAY_SIZING_GROW(0), .height = CLAY_SIZING_FIXED(80) },
-                            .padding = CLAY_PADDING_ALL(16),
-                            .childGap = 16 },
+                            .padding = CLAY_PADDING_ALL(0),
+                            .childGap = 16,
+                            .childAlignment = { .y = CLAY_ALIGN_Y_CENTER }
+                        },
                 .backgroundColor = HEADER_COLOR }) {
+                    CLAY(CLAY_ID("ProfilePicture"), {
+                            .layout = {
+                                .sizing = {
+                                    .width = CLAY_SIZING_FIXED(70),
+                                    .height = CLAY_SIZING_FIXED(86) }
+                              },
+                            .image = { .imageData = "resources/leroy.png" } }) {
 
+                    }
+                    CLAY_TEXT(CLAY_STRING("TEENY BUG"), { .fontSize = 24, .textColor = {255, 255, 255, 255} });
             }
 
             CLAY(CLAY_ID("AsmContainer"), asmContainerConfig) {
