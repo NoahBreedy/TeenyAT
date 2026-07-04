24b8142d91829569a0dcfdc2c2dc87d71ffd84e6
Noah Breedy <noahfamily373@gmail.com>
2026-07-04 02:06:01 -0400

    tbug: added functionality to header buttons

--- tbug/include/gui/header_window.h
+++ tbug/include/gui/header_window.h
@@ -6,6 +6,10 @@
 #include "clay.h"
 #include "clay_colors.h"
 
+extern bool stop_mode;
+extern bool step_mode;
+extern bool reset_mode;
+
 void HeaderWindow();
 
 #endif /* __HEADER_WINDOW__ */
--- tbug/src/gui/header_window.c
+++ tbug/src/gui/header_window.c
@@ -1,5 +1,28 @@
 #include "gui/header_window.h"
 
+static uint8_t action_ids[3] = {0, 1, 2};
+
+bool stop_mode  = true;
+bool step_mode  = false;
+bool reset_mode = false;
+
+void HandleActionBoxInteraction(Clay_ElementId elementId, Clay_PointerData pointerInfo, void *userData) {
+    uint8_t actionID = *((uint8_t*)userData);
+
+    // Pointer state allows you to detect mouse down / hold / release
+    if (pointerInfo.state == CLAY_POINTER_DATA_PRESSED_THIS_FRAME) {
+        
+        if(actionID == 0) { // RUN or STOP
+            stop_mode = !stop_mode; 
+        }else if(actionID == 1) { // STEP
+            step_mode = true; 
+        }else { // RESET
+            reset_mode = true; 
+        }
+
+    }
+}
+
 Clay_ElementDeclaration actionBoxConfig = (Clay_ElementDeclaration) {
     .layout = { .layoutDirection = CLAY_LEFT_TO_RIGHT,
                 .sizing = { .width = CLAY_SIZING_FIXED(200),
@@ -10,9 +33,11 @@ Clay_ElementDeclaration actionBoxConfig = (Clay_ElementDeclaration) {
     .backgroundColor = ACTION_BOX_COLOR
 };
 
-void ActionBoxComponent(const char* my_str, char* img_url) {
+void ActionBoxComponent(const char* my_str, char* img_url, uint8_t* component) {
     Clay_String text = (Clay_String){.isStaticallyAllocated = true, .length = strlen(my_str), .chars = my_str};
     CLAY_AUTO_ID(actionBoxConfig) {
+        Clay_OnHover(HandleActionBoxInteraction, component);
+
         CLAY_AUTO_ID({
                     .layout = {
                         .sizing = {
@@ -44,14 +69,17 @@ void HeaderWindow() {
                         },
                     .image = { .imageData = "resources/leroy.png" } }) {
             }
-
+            
             CLAY_TEXT(CLAY_STRING("TEENY BUG"), { .fontSize = 24, .textColor = {255, 255, 255, 255} });
             
-            ActionBoxComponent("Run", "resources/Run_Button.png");
+            if(stop_mode)
+                ActionBoxComponent("Run", "resources/Run_Button.png", action_ids);
+            else
+                ActionBoxComponent("Stop", "resources/Stop_Button.png", action_ids);
 
-            ActionBoxComponent("Step", "resources/Step_Button.png");
+            ActionBoxComponent("Step", "resources/Step_Button.png", action_ids + 1);
 
-            ActionBoxComponent("Reset", "resources/Reset_Button.png");
+            ActionBoxComponent("Reset", "resources/Reset_Button.png", action_ids + 2);
 
     }
 }
